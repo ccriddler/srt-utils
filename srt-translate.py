35d3212ed68ee61391febfcc875703c64f74fa58
ccriddler <ccriddler@protonmail.com>
2023-02-23 23:48:28 -0800

    made scripts executable. added deepl support to srt-translate

--- srt-translate.py
+++ srt-translate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-#import deepl
+import deepl
 import googletrans
 import srt
 import time
@@ -9,13 +9,15 @@ import os
 
 parser = argparse.ArgumentParser(description="An auto translator for .srt files utilizing google's translate API")
 parser.add_argument("srt", help="the .srt file to translate")
-parser.add_argument("--lang-from", default="auto", help="Source language of the .srt file")
-parser.add_argument("--lang-to", default="en", help="Dest language of the output .srt file")
+parser.add_argument("--lang-from", default="JA", help="Source language of the .srt file")
+parser.add_argument("--lang-to", default="EN-US", help="Dest language of the output .srt file")
 parser.add_argument("--delay", default=1, type=int, help="Delay time between translation api requests")
-parser.add_argument("--auth", help="Auth key for your account, if using deepl API (WIP)")
+parser.add_argument("--deepl-auth", help="Auth key for your account, if using deepl API (WIP)")
 args = parser.parse_args()
 
 translator = googletrans.Translator()
+if(args.deepl_auth):
+    translator = deepl.Translator(args.deepl_auth)
 
 def read_text(path):
     with open(path, "r") as buf:
@@ -28,12 +30,21 @@ def write_text(path, data):
 
 subs = srt.parse(read_text(args.srt))
 
-def process_sub(sub):
-    return translator.translate(
-        text=sub.content,
-        dest=args.lang_to,
-        src=args.lang_from
-    ).text
+def process_sub(tl, sub):
+    if(isinstance(tl, googletrans.Translator)):
+        return tl.translate(
+            text=sub.content,
+            dest=args.lang_to,
+            src=args.lang_from
+        ).text
+
+    if(isinstance(tl, deepl.Translator)):
+        return tl.translate_text(
+            sub.content,
+            target_lang=args.lang_to,
+            source_lang=args.lang_from
+        ).text
+    return ""
 
 print("TRANSLATION BEGIN '{}'".format(args.srt))
 trans_subs = []
@@ -46,7 +57,7 @@ for idx, sub in enumerate(sublist):
     print("---")
 
     print("In -> {}".format(sub.content))
-    trans_subs[-1].content = process_sub(sub)
+    trans_subs[-1].content = process_sub(translator, sub)
     print("Out -> {}".format(trans_subs[-1].content))
 
     print("")
